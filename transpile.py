86c957029d0a00441117cf8597572c7c9e4f7f0f
maddyonline <madhavjha@gmail.com>
2016-11-09 18:54:32 -0800

    more

--- transpile.py
+++ transpile.py
@@ -3,7 +3,9 @@ def type_of(expr):
         return 'FIELD_LOOKUP'
     if type(expr) in (int, float, str, type(None)):
         return 'LITERAL'
-    if type(expr) == list and len(expr) > 0:
+    if type(expr) == list and len(expr) == 2:
+        return 'UNARY_OPERATION'
+    if type(expr) == list and len(expr) == 3:
         return 'BINARY_OPERATION'
 
 
@@ -24,6 +26,13 @@ def evaluate(fields, expr, depth=0):
         return eval_literal(fields, expr)
     elif type_of_expr == 'FIELD_LOOKUP':
         return eval_field(fields, expr)
+    elif type_of_expr == 'UNARY_OPERATION':
+        computed = evaluate(fields, expr[1])
+        d = {
+            'is_empty': 'IS NULL',
+            'is_non_empty': 'IS NOT NULL',
+        }
+        return '{} {}'.format(computed, d[expr[0]])
     elif type_of_expr == 'BINARY_OPERATION':
         op, lhs, rhs = expr[0], expr[1], expr[2]
         lhs_str = evaluate(fields, lhs, depth+1)
@@ -50,7 +59,8 @@ def tests():
         ["OR", ["!=", ["field", 3], "2015-11-01"], ["=", ["field", 1], 456]],
         ["AND", 
          ["!=", ["field", 3], None], 
-         ["OR", [">", ["field", 4], 25], ["=", ["field", 2], "Jerry"]]]
+         ["OR", [">", ["field", 4], 25], ["=", ["field", 2], "Jerry"]]],
+         ["is_empty", ["field", 3]],
     ]
     for expr in expressions:
         e = evaluate(fields, expr)
