4058b55a9ef0b0a04d1f6805826319a6e1166d8e
maddyonline <madhavjha@gmail.com>
2016-11-09 18:57:18 -0800

    more

--- transpile.py
+++ transpile.py
@@ -7,7 +7,7 @@ def type_of(expr):
         return 'UNARY_OPERATION'
     if type(expr) == list and len(expr) == 3:
         return 'BINARY_OPERATION'
-
+    return 'MULTIARY_OPERATION'
 
 def eval_field(fields, expr):
     return fields[expr[1]]
@@ -44,6 +44,8 @@ def evaluate(fields, expr, depth=0):
         else:
             op_str = op
         return '{} {} {}'.format(lhs_str, op_str, rhs_str)
+    op_str = ' {} '.format(expr[0])
+    return op_str.join([evaluate(fields, e) for e in expr[1:]])
 
 def tests():
     fields = {
@@ -58,7 +60,8 @@ def tests():
         ["AND", ["<", ["field", 1],  5], ["=", ["field", 2], "joe"]],
         ["OR", ["!=", ["field", 3], "2015-11-01"], ["=", ["field", 1], 456]],
         ["AND", 
-         ["!=", ["field", 3], None], 
+         ["!=", ["field", 3], None],
+         ["!=", ["field", 2], None], 
          ["OR", [">", ["field", 4], 25], ["=", ["field", 2], "Jerry"]]],
          ["is_empty", ["field", 3]],
     ]
